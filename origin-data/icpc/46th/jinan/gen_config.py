b29ce2b8f1cfd2e20d6bf323a5b56b196633eb70
Dup4 <lyuzhi.pan@gmail.com>
2021-11-14 20:58:26 +0800

    feat: icpc 46th jinan

--- origin-data/icpc/46th/jinan/gen_config.py
+++ origin-data/icpc/46th/jinan/gen_config.py
@@ -76,9 +76,9 @@ status_time_display = {
 
 medal = {
     "official": {
-        'gold': 30,
-        'silver': 60,
-        'bronze': 90,
+        'gold': 35,
+        'silver': 70,
+        'bronze': 105,
     }
 }
 
@@ -92,7 +92,7 @@ config = {
     'organization': 'School',
     'status_time_display': status_time_display,
     'penalty': 20 * 60,
-    # 'medal': medal,
+    'medal': medal,
     'balloon_color': generate_balloon_color(problem_num),
 }
 
