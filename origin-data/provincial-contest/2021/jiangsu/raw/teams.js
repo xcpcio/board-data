b3c2304541a9a6d77414d25f9dc27ee885ffa32b
Dup4 <lyuzhi.pan@gmail.com>
2021-12-25 17:18:49 +0800

    feat: 2021 jiangsu cpc data

---
+++ origin-data/provincial-contest/2021/jiangsu/raw/teams.js
@@ -0,0 +1,873 @@
+var teams = {
+	"1": {
+			"members": "郭仕杰、陈昭、张明旭",
+			"school": "常熟理工学院",
+			"team": "卷王的心得感悟",
+			"type": "type2"
+		},
+	"2": {
+			"members": "张啸、张羽飏、陈佳鸿",
+			"school": "常熟理工学院",
+			"team": "键盘里有虫",
+			"type": "type2"
+		},
+	"3": {
+			"members": "曾谷优、须迦成、张新纪",
+			"school": "常熟理工学院",
+			"team": "AC三题就罢工",
+			"type": "type2"
+		},
+	"4": {
+			"members": "姚康政、常博豪、严俊华",
+			"school": "常熟理工学院",
+			"team": "曙光",
+			"type": "type2"
+		},
+	"5": {
+			"members": "徐柔、张雷、詹智航",
+			"school": "常熟理工学院",
+			"team": "拿不拿奖队长都女装",
+			"type": "type2"
+		},
+	"6": {
+			"members": "陈滟婧、段梦雨、林涵",
+			"school": "常熟理工学院",
+			"team": "我们发量多着呢",
+			"type": "type1 girls"
+		},
+	"7": {
+			"members": "俞戴葳、张顺楷、叶正杰",
+			"school": "常熟理工学院",
+			"team": "离离原上普",
+			"type": "type3 unofficial"
+		},
+	"8": {
+			"members": "杨淳智、邹静、卓曼",
+			"school": "常熟理工学院",
+			"team": "一起吃奶酪",
+			"type": "type3 unofficial"
+		},
+	"9": {
+			"members": "王宇航、王子文、金豪楠",
+			"school": "常熟理工学院",
+			"team": "烧烤队",
+			"type": "type3 unofficial"
+		},
+	"10": {
+			"members": "刘朗麒、陈哲斯、薛江天",
+			"school": "东南大学",
+			"team": "/all",
+			"type": "type2"
+		},
+	"11": {
+			"members": "郑祎凡、潘昱辰、余晨曦",
+			"school": "东南大学",
+			"team": "动力强劲哈库鱼",
+			"type": "type2"
+		},
+	"12": {
+			"members": "黄天宇、张皓、田昊冬",
+			"school": "东南大学",
+			"team": "未命名",
+			"type": "type2"
+		},
+	"13": {
+			"members": "赵祖妍、王玉珏、靳佳琪",
+			"school": "东南大学",
+			"team": "程序媛",
+			"type": "type1 girls"
+		},
+	"14": {
+			"members": "许正正、程鑫宇、李伟嘉",
+			"school": "河海大学",
+			"team": "河海铁匠铺",
+			"type": "type2"
+		},
+	"15": {
+			"members": "张时钊、郁冰哲、余一鸣",
+			"school": "河海大学",
+			"team": "可每队",
+			"type": "type2"
+		},
+	"16": {
+			"members": "蒋子俊、邓伦、占羽淳",
+			"school": "河海大学",
+			"team": "几丁质",
+			"type": "type2"
+		},
+	"17": {
+			"members": "高康悦、张星宇、肖潇",
+			"school": "河海大学",
+			"team": "埃派克斯",
+			"type": "type2"
+		},
+	"18": {
+			"members": "颜小涵、何瑾雨、王竞同",
+			"school": "河海大学",
+			"team": "焦糖布丁",
+			"type": "type2"
+		},
+	"19": {
+			"members": "徐尚、丁佳锋、陈昊宇",
+			"school": "河海大学常州校区",
+			"team": "哇哇哇",
+			"type": "type2"
+		},
+	"20": {
+			"members": "俞天骋、孙嘉昊、王英吉",
+			"school": "河海大学常州校区",
+			"team": "几何杀手",
+			"type": "type2"
+		},
+	"21": {
+			"members": "李欣宇、陈宇晨、卜乐天",
+			"school": "江南大学",
+			"team": "我要学离散",
+			"type": "type2"
+		},
+	"22": {
+			"members": "徐润天、杜昊、朱文涛",
+			"school": "江南大学",
+			"team": "我和铁牌过圣诞",
+			"type": "type2"
+		},
+	"23": {
+			"members": "何云康、张逍遥、陈云豪",
+			"school": "江南大学",
+			"team": "远坂凛粉丝队",
+			"type": "type2"
+		},
+	"24": {
+			"members": "祝锡平、方志学、张浩东",
+			"school": "江苏大学",
+			"team": "江苏大学猛1队",
+			"type": "type2"
+		},
+	"25": {
+			"members": "王嘉诚、姜辰、林海昊",
+			"school": "江苏大学",
+			"team": "魔术棒",
+			"type": "type2"
+		},
+	"26": {
+			"members": "侯鹏飞、邸凯、胡森",
+			"school": "江苏海洋大学",
+			"team": "江海大一队",
+			"type": "type2"
+		},
+	"27": {
+			"members": "邢立豹、刘中原、沈天烨",
+			"school": "江苏海洋大学",
+			"team": "开拓者",
+			"type": "type2"
+		},
+	"28": {
+			"members": "林点、周瑞、郑舒洋",
+			"school": "江苏科技大学",
+			"team": "队友好码",
+			"type": "type2"
+		},
+	"29": {
+			"members": "罗志康、刘兴源、殷苏海",
+			"school": "江苏科技大学",
+			"team": "球球队友再A一题",
+			"type": "type2"
+		},
+	"30": {
+			"members": "盛夏滔、范林赟、周煜斌",
+			"school": "金陵科技学院",
+			"team": "布洛特亨德尔",
+			"type": "type2"
+		},
+	"31": {
+			"members": "殷俊杰、张峰、李蕴杰",
+			"school": "金陵科技学院",
+			"team": "血压太高控制不住怎么办",
+			"type": "type2"
+		},
+	"32": {
+			"members": "万卓越、陈峻、帅蕴哲",
+			"school": "金陵科技学院",
+			"team": "打铁星星队",
+			"type": "type2"
+		},
+	"33": {
+			"members": "王尽欢、黄文彬、商颖雷",
+			"school": "金陵科技学院",
+			"team": "透视",
+			"type": "type2"
+		},
+	"34": {
+			"members": "陆青阳、杜星语、吴煜青",
+			"school": "南京大学",
+			"team": "admax",
+			"type": "type2"
+		},
+	"35": {
+			"members": "丁天行、单启程、欧丰宁",
+			"school": "南京大学",
+			"team": "虚幻黄昏",
+			"type": "type2"
+		},
+	"36": {
+			"members": "曹逸中、陈弘毅、邹忠奇",
+			"school": "南京大学",
+			"team": "如烟",
+			"type": "type2"
+		},
+	"37": {
+			"members": "李博颖、蔡之恒、熊丘桓",
+			"school": "南京大学",
+			"team": "鸽屋咕",
+			"type": "type2"
+		},
+	"38": {
+			"members": "林荣恩、张浩宇、陈晓",
+			"school": "南京大学",
+			"team": "Thedereap",
+			"type": "type2"
+		},
+	"39": {
+			"members": "陈佳庚、单博、回溯",
+			"school": "南京大学",
+			"team": "不坠机就算胜利",
+			"type": "type3 unofficial"
+		},
+	"40": {
+			"members": "王淳扬、张昕渊、冉雨杭",
+			"school": "南京大学",
+			"team": "白日",
+			"type": "type3 unofficial"
+		},
+	"41": {
+			"members": "王恺予、张乃仁、王锦朋",
+			"school": "南京大学",
+			"team": "还没想好叫什么",
+			"type": "type3 unofficial"
+		},
+	"42": {
+			"members": "水兵、陈元旻、蒋潇鹏",
+			"school": "南京大学",
+			"team": "烂飞机",
+			"type": "type3 unofficial"
+		},
+	"43": {
+			"members": "柴浩天、陈荣锴、贺柄毓",
+			"school": "南京大学",
+			"team": "菜根队",
+			"type": "type3 unofficial"
+		},
+	"44": {
+			"members": "周相羽、朱家辰、贺卓宇",
+			"school": "南京大学",
+			"team": "南小饱",
+			"type": "type3 unofficial"
+		},
+	"45": {
+			"members": "渠宇、张鸿飞、龚培昊",
+			"school": "南京大学金陵学院",
+			"team": "海阔天空",
+			"type": "type2"
+		},
+	"46": {
+			"members": "汪有标、王佳鑫、张华涛",
+			"school": "南京工业大学",
+			"team": "南工大n队",
+			"type": "type2"
+		},
+	"47": {
+			"members": "卢漫可、张林骏、廖志翔",
+			"school": "南京工业大学",
+			"team": "非全菜",
+			"type": "type2"
+		},
+	"48": {
+			"members": "顾喜龙、宣伟康、杨一",
+			"school": "南京工业大学",
+			"team": "给评测姬倒一杯卡布奇诺",
+			"type": "type2"
+		},
+	"49": {
+			"members": "赵凯、何海嘉、李园章",
+			"school": "南京工业大学",
+			"team": "打铁小分队",
+			"type": "type2"
+		},
+	"50": {
+			"members": "张喆、卫麓飞、张逸飞",
+			"school": "南京工业大学",
+			"team": "代码都",
+			"type": "type2"
+		},
+	"51": {
+			"members": "孔得全、潘佳辉、刘屹林",
+			"school": "南京航空航天大学",
+			"team": "被迫打铁",
+			"type": "type2"
+		},
+	"52": {
+			"members": "吕俊琪、姚有健、张一帆",
+			"school": "南京航空航天大学",
+			"team": "教练我想打电竞",
+			"type": "type2"
+		},
+	"53": {
+			"members": "周傅毅楠、钱增辉、刘昊哲",
+			"school": "南京航空航天大学",
+			"team": "幺鸡小分队",
+			"type": "type2"
+		},
+	"54": {
+			"members": "张兆骞、徐岩心、孙海龙",
+			"school": "南京航空航天大学",
+			"team": "拿不到金牌也有女朋友队",
+			"type": "type2"
+		},
+	"55": {
+			"members": "钱品亦、李亚宁、岑嘉诚",
+			"school": "南京航空航天大学",
+			"team": "在JSCPC中寻求AC是否搞错了什么？",
+			"type": "type2"
+		},
+	"56": {
+			"members": "袁瑜谦、陆晓兰、张馨予",
+			"school": "南京航空航天大学",
+			"team": "匈牙利牛肉焗饭",
+			"type": "type1 girls"
+		},
+	"57": {
+			"members": "刘胡东、杜佳睿、祖元琨",
+			"school": "南京航空航天大学",
+			"team": "三脸懵逼",
+			"type": "type3 unofficial"
+		},
+	"58": {
+			"members": "周栎、丁希宸、李嘉航",
+			"school": "南京航空航天大学",
+			"team": "打代码会生发",
+			"type": "type3 unofficial"
+		},
+	"59": {
+			"members": "陈一涵、代领、谢东娜",
+			"school": "南京理工大学",
+			"team": "涅槃饼干",
+			"type": "type2"
+		},
+	"60": {
+			"members": "刘星辰、王瀚楠、季若曦",
+			"school": "南京理工大学",
+			"team": "瑟瑟发抖",
+			"type": "type2"
+		},
+	"61": {
+			"members": "陈涣焕、金朝哲、郭佳樊",
+			"school": "南京理工大学",
+			"team": "逆转猫猫头",
+			"type": "type2"
+		},
+	"62": {
+			"members": "肖冉、王浩宇、蔡佳铭",
+			"school": "南京理工大学",
+			"team": "点燃猫猫头",
+			"type": "type2"
+		},
+	"63": {
+			"members": "陆宗泽、潘玉栋、谢石林",
+			"school": "南京理工大学",
+			"team": "草莓果冻",
+			"type": "type2"
+		},
+	"64": {
+			"members": "周怿翔、郑镐予、张友鑫",
+			"school": "南京林业大学",
+			"team": "崩坏世界的歌姬",
+			"type": "type2"
+		},
+	"65": {
+			"members": "马维龙、李贾、张刘杰",
+			"school": "南京林业大学",
+			"team": "彳亍",
+			"type": "type2"
+		},
+	"66": {
+			"members": "江涵非、王易涛、蔡宇辰",
+			"school": "南京林业大学",
+			"team": "快乐的小鸟",
+			"type": "type2"
+		},
+	"67": {
+			"members": "邢超宇、雷宇、葛文轩",
+			"school": "南京林业大学",
+			"team": "又来取名",
+			"type": "type2"
+		},
+	"68": {
+			"members": "吴雨、龚成华、樊世雄",
+			"school": "南京师范大学",
+			"team": "孙伯符开大快上船",
+			"type": "type2"
+		},
+	"69": {
+			"members": "周阳、宗煜、李仲恒",
+			"school": "南京师范大学",
+			"team": "不打铁的只狼龙之返乡",
+			"type": "type2"
+		},
+	"70": {
+			"members": "沈钰东、陈晓健、沈泽波",
+			"school": "南京师范大学",
+			"team": "你单身还不打比赛干嘛",
+			"type": "type2"
+		},
+	"71": {
+			"members": "彭章荣、宋廉鸿、李睿",
+			"school": "南京师范大学",
+			"team": "苏大食堂好吃吗",
+			"type": "type2"
+		},
+	"72": {
+			"members": "周青、田梦琪、秦世玉",
+			"school": "南京师范大学",
+			"team": "必有我师焉队",
+			"type": "type2"
+		},
+	"73": {
+			"members": "李云倩、王聪瑶、雷奕",
+			"school": "南京师范大学",
+			"team": "翻山越岭来打你",
+			"type": "type1 girls"
+		},
+	"74": {
+			"members": "卜阳、孙陈浩南、张育林",
+			"school": "南京晓庄学院",
+			"team": "某科学的A一题就对",
+			"type": "type2"
+		},
+	"75": {
+			"members": "姜涵、伍锦融、陶志诚",
+			"school": "南京晓庄学院",
+			"team": "海王养山猪",
+			"type": "type2"
+		},
+	"76": {
+			"members": "徐志辉、龚子莛、睢志翔",
+			"school": "南京晓庄学院",
+			"team": "watermelon黄瓜队",
+			"type": "type2"
+		},
+	"77": {
+			"members": "孙星宇、洪湘雪、朱媛媛",
+			"school": "南京晓庄学院",
+			"team": "今晚不许emo队",
+			"type": "type2"
+		},
+	"78": {
+			"members": "伍雅靖、胡颖、单宇星",
+			"school": "南京晓庄学院",
+			"team": "上面的别A了",
+			"type": "type1 girls"
+		},
+	"79": {
+			"members": "董兆琦、刘明扬、洪伟",
+			"school": "南京信息工程大学",
+			"team": "跟不上榜",
+			"type": "type2"
+		},
+	"80": {
+			"members": "王梓宇、张昱昊、管润达",
+			"school": "南京信息工程大学",
+			"team": "从零开始的ACM生活",
+			"type": "type2"
+		},
+	"81": {
+			"members": "陈泳琦、杨可、陈超然",
+			"school": "南京信息工程大学",
+			"team": "专业防爆零",
+			"type": "type2"
+		},
+	"82": {
+			"members": "马良、张兴洋、吴昊垲",
+			"school": "南京信息工程大学",
+			"team": "璃月之夜",
+			"type": "type2"
+		},
+	"83": {
+			"members": "王营祺、周恒康、杨盛凯",
+			"school": "南京信息工程大学",
+			"team": "小小甜心",
+			"type": "type2"
+		},
+	"84": {
+			"members": "邹佳云、金舒文、马钟蕙",
+			"school": "南京信息工程大学",
+			"team": "AutoResident",
+			"type": "type1 girls"
+		},
+	"85": {
+			"members": "张禹健、文超航、冯一帆",
+			"school": "南京邮电大学",
+			"team": "收收心找个电子厂上班了",
+			"type": "type2"
+		},
+	"86": {
+			"members": "郁青陟、肖鸿、陆晓琦",
+			"school": "南京邮电大学",
+			"team": "蜜雪去冰城",
+			"type": "type2"
+		},
+	"87": {
+			"members": "李清远、金益凡、蔡振川",
+			"school": "南京邮电大学",
+			"team": "哦对对对哦不对不对",
+			"type": "type2"
+		},
+	"88": {
+			"members": "张帅青、刁煜凡、王一权",
+			"school": "南京邮电大学",
+			"team": "事末鸽者",
+			"type": "type2"
+		},
+	"89": {
+			"members": "何乐晗、陈以信、赵一舟",
+			"school": "南京邮电大学",
+			"team": "Citrus",
+			"type": "type2"
+		},
+	"90": {
+			"members": "祁佳怡、刘嘉怡、陶美妍",
+			"school": "南京邮电大学",
+			"team": "咱家要有钱了",
+			"type": "type1 girls"
+		},
+	"91": {
+			"members": "黄锦宇、吴思余、马青宇",
+			"school": "南京邮电大学",
+			"team": "饥饿的时候没有创造力",
+			"type": "type3 unofficial"
+		},
+	"92": {
+			"members": "宋辰龙、蒋锐、卢雨田",
+			"school": "南京邮电大学",
+			"team": "白给队",
+			"type": "type3 unofficial"
+		},
+	"93": {
+			"members": "金心悦、沈瑾麟、葛涛宁",
+			"school": "南京邮电大学通达学院",
+			"team": "下次一定",
+			"type": "type2"
+		},
+	"94": {
+			"members": "代国豪、魏冯婧、马宁",
+			"school": "南京邮电大学通达学院",
+			"team": "小霸王算法机",
+			"type": "type2"
+		},
+	"95": {
+			"members": "钱未闻、夏天、鲍昱丞",
+			"school": "南京邮电大学通达学院",
+			"team": "重返艾泽拉斯",
+			"type": "type2"
+		},
+	"96": {
+			"members": "王钲钧、王以宁、武艺成",
+			"school": "南京邮电大学通达学院",
+			"team": "啥都行哈哈",
+			"type": "type2"
+		},
+	"97": {
+			"members": "李志康、张帅、杨阳",
+			"school": "南京中医药大学",
+			"team": "欢乐斗地组",
+			"type": "type2"
+		},
+	"98": {
+			"members": "佘涛、王栋城、王文博",
+			"school": "南京中医药大学",
+			"team": "一起编程",
+			"type": "type2"
+		},
+	"99": {
+			"members": "王浩任、孙长江、史铭东",
+			"school": "南通大学",
+			"team": "图灵说得队",
+			"type": "type2"
+		},
+	"100": {
+			"members": "苏展、叶盛、季炎",
+			"school": "南通大学",
+			"team": "NTU1",
+			"type": "type2"
+		},
+	"101": {
+			"members": "缪芸、李佳帆、张世梁",
+			"school": "南通大学",
+			"team": "快乐星球",
+			"type": "type2"
+		},
+	"102": {
+			"members": "吴高杰、周杰、韩愈",
+			"school": "南通大学",
+			"team": "集思广益一波",
+			"type": "type2"
+		},
+	"103": {
+			"members": "缪祺、蔡巧颖、钱卢骏",
+			"school": "南通大学",
+			"team": "NTUUUU",
+			"type": "type2"
+		},
+	"104": {
+			"members": "李柯辰、徐晓涵、张晓宇",
+			"school": "南通师范高等专科学校",
+			"team": "lkc说得都对",
+			"type": "type2"
+		},
+	"105": {
+			"members": "许刘莲、张豫、徐文泓",
+			"school": "南通师范高等专科学校",
+			"team": "Best effort",
+			"type": "type2"
+		},
+	"106": {
+			"members": "朱梦雨、陈彬彬、郭嘉美",
+			"school": "南通师范高等专科学校",
+			"team": "The One",
+			"type": "type2"
+		},
+	"107": {
+			"members": "林志诚、顾俊辉、陈惟一",
+			"school": "苏州大学",
+			"team": "555我的炸鸡",
+			"type": "type2"
+		},
+	"108": {
+			"members": "周进一、韩则进、肖锦嗣",
+			"school": "苏州大学",
+			"team": "求转计科院",
+			"type": "type2"
+		},
+	"109": {
+			"members": "于立恒、陆佳浩、王志庞",
+			"school": "苏州大学",
+			"team": "渐入'佳'境",
+			"type": "type2"
+		},
+	"110": {
+			"members": "王雨、孙楠、汤添文",
+			"school": "苏州大学",
+			"team": "喷射战士",
+			"type": "type2"
+		},
+	"111": {
+			"members": "丁誉洋、孙孜斌、殷九思",
+			"school": "苏州大学",
+			"team": "咕咕咕咕咕咕咕",
+			"type": "type2"
+		},
+	"112": {
+			"members": "赵若愚、侯熙致、简嘉瑶",
+			"school": "苏州大学",
+			"team": "九二一",
+			"type": "type1 girls"
+		},
+	"113": {
+			"members": "冯泽森、王品正、周泓余",
+			"school": "苏州大学",
+			"team": "赶紧补题去吧",
+			"type": "type3 unofficial"
+		},
+	"114": {
+			"members": "黄学文、许睿阳、唐瑞珑",
+			"school": "苏州大学",
+			"team": "气球多多",
+			"type": "type3 unofficial"
+		},
+	"115": {
+			"members": "郭沈琳、尹邓阳、李承庚",
+			"school": "苏州大学",
+			"team": "AD钙奶队",
+			"type": "type3 unofficial"
+		},
+	"116": {
+			"members": "刘瀚文、张浩然、陈景尧",
+			"school": "苏州大学",
+			"team": "零带三",
+			"type": "type3 unofficial"
+		},
+	"117": {
+			"members": "杨宇博、严正安、陈琦",
+			"school": "苏州大学",
+			"team": "只想去刷题",
+			"type": "type3 unofficial"
+		},
+	"118": {
+			"members": "柯骅、陈夫瑞、杨哲元",
+			"school": "苏州大学",
+			"team": "哪里签到啊",
+			"type": "type3 unofficial"
+		},
+	"119": {
+			"members": "周夏卿、何宇涛、陈泽豪",
+			"school": "苏州大学",
+			"team": "永不摆烂队",
+			"type": "type3 unofficial"
+		},
+	"120": {
+			"members": "郭瑞阳、曹鹏、沈广浩",
+			"school": "苏州科技大学",
+			"team": "青钢影说她下次来",
+			"type": "type2"
+		},
+	"121": {
+			"members": "吴晓凡、高成杰、韩世豪",
+			"school": "苏州科技大学",
+			"team": "暴风雨破碎机",
+			"type": "type2"
+		},
+	"122": {
+			"members": "蒋城、顾迅、彭羽",
+			"school": "苏州科技大学",
+			"team": "笨小孩",
+			"type": "type2"
+		},
+	"123": {
+			"members": "时微淳、黄泱邦、孙宇航",
+			"school": "苏州科技大学",
+			"team": "算法指难",
+			"type": "type2"
+		},
+	"124": {
+			"members": "刘奕州、邹龙康、武家辉",
+			"school": "苏州科技大学",
+			"team": "像风一样",
+			"type": "type2"
+		},
+	"125": {
+			"members": "张锋、张恒、刘通",
+			"school": "徐州工程学院",
+			"team": "啪的一下就过了",
+			"type": "type2"
+		},
+	"126": {
+			"members": "陈逸桐、陈金保、赵雅雯",
+			"school": "扬州大学",
+			"team": "摆就完了呗",
+			"type": "type2"
+		},
+	"127": {
+			"members": "王逸濠、王进、王天也",
+			"school": "扬州大学",
+			"team": "随便",
+			"type": "type2"
+		},
+	"128": {
+			"members": "周洲、张昱、单文婧",
+			"school": "扬州大学",
+			"team": "三分糖",
+			"type": "type1 girls"
+		},
+	"129": {
+			"members": "周玲霞、陈家豪、彭明辉",
+			"school": "扬州大学",
+			"team": "码农队",
+			"type": "type3 unofficial"
+		},
+	"130": {
+			"members": "孙宇晨、罗浩巍、张士豪",
+			"school": "中国矿业大学",
+			"team": "你说的都队",
+			"type": "type2"
+		},
+	"131": {
+			"members": "王永康、李博宇、蔡家桢",
+			"school": "中国矿业大学",
+			"team": "面向样例编程",
+			"type": "type2"
+		},
+	"132": {
+			"members": "宋明潇、杨斌涵、钟泽旭",
+			"school": "中国矿业大学",
+			"team": "一般性随机忆墨人",
+			"type": "type2"
+		},
+	"133": {
+			"members": "温耀林、尉高耀、杨忻至",
+			"school": "中国矿业大学",
+			"team": "签到题敲舞蹈链",
+			"type": "type2"
+		},
+	"134": {
+			"members": "周俊峰、肖扬航、雷毅",
+			"school": "中国矿业大学",
+			"team": "塞尔达",
+			"type": "type2"
+		},
+	"135": {
+			"members": "邱雪琳、张园惠、罗纯颖",
+			"school": "中国矿业大学",
+			"team": "三只松鼠",
+			"type": "type1 girls"
+		},
+	"136": {
+			"members": "刘一鸣、郁乔霖、卜勋",
+			"school": "西交利物浦大学",
+			"team": "西浦AC小分队",
+			"type": "type2"
+		},
+	"137": {
+			"members": "陶轶凡、尤存浩、文天禧",
+			"school": "西交利物浦大学",
+			"team": "叮叮车",
+			"type": "type2"
+		},
+	"138": {
+			"members": "谢翔、李关宝、邱宇",
+			"school": "西交利物浦大学",
+			"team": "嘉然小姐的猫",
+			"type": "type2"
+		},
+	"139": {
+			"members": "陈盈希、王小丫、董紫菱",
+			"school": "西交利物浦大学",
+			"team": "勇敢牛牛不怕困难",
+			"type": "type1 girls"
+		},
+	"140": {
+			"members": "朱宸远、沈开嵘、钟逸轩",
+			"school": "苏州中学生",
+			"team": "季姬击鸡记",
+			"type": "type3 unofficial"
+		},
+	"141": {
+			"members": "庹澔、余忱恂、周呈毅",
+			"school": "苏州中学生",
+			"team": "石室施氏食狮史",
+			"type": "type3 unofficial"
+		},
+	"142": {
+			"members": "曹宇辰、赵奕学、徐佳亿",
+			"school": "苏州中学生",
+			"team": "东郭先生爱吃狼",
+			"type": "type3 unofficial"
+		},
+	"143": {
+			"members": "胡哲瀚、杨子墨、李瑾芃",
+			"school": "苏州中学生",
+			"team": "神秘的金鸡湖水怪",
+			"type": "type3 unofficial"
+		},
+	"144": {
+			"members": "张轩扬、王若璞、刘奕斌",
+			"school": "苏州中学生",
+			"team": "彭城百年羊肉汤",
+			"type": "type3 unofficial"
+		},
+	"145": {
+			"members": "王居瑶、陈睿霖、翟华杰",
+			"school": "苏州中学生",
+			"team": "三日凌空",
+			"type": "type3 unofficial"
+		},
+	};
+	
