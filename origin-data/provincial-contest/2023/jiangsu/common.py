1bad824d21aeede36056484974fb3cbbdfc2d495
Dup4 <lyuzhi.pan@gmail.com>
2023-05-27 00:51:20 +0800

    feat: 2023 jiangsu cpc config
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/provincial-contest/2023/jiangsu/common.py
@@ -0,0 +1,55 @@
+import time
+import os
+import json
+
+from xcpcio_board_spider import logger, Contest, constants, logo, utils
+
+log = logger.init_logger()
+
+
+def get_basic_contest():
+    c = Contest()
+
+    c.frozen_time = 60 * 60
+    c.penalty = 20 * 60
+    c.organization = "School"
+
+    c.group = {
+        constants.TEAM_TYPE_OFFICIAL: constants.TEAM_TYPE_ZH_CN_OFFICIAL,
+        constants.TEAM_TYPE_UNOFFICIAL: constants.TEAM_TYPE_ZH_CH_UNOFFICIAL,
+        constants.TEAM_TYPE_GIRL: constants.TEAM_TYPE_ZH_CH_GIRL,
+    }
+
+    c.status_time_display = {
+        constants.RESULT_CORRECT: 1,
+        constants.RESULT_INCORRECT: 1,
+        constants.RESULT_PENDING: 1,
+    }
+
+    # c.logo = logo.CCPC
+
+    return c
+
+
+def work(c: Contest, data_dir: str, fetch_uri: str):
+    utils.ensure_makedirs(data_dir)
+    utils.output(os.path.join(data_dir, "config.json"), c.get_dict)
+    utils.output(os.path.join(data_dir, "team.json"), {}, True)
+    utils.output(os.path.join(data_dir, "run.json"), [], True)
+
+    # with open("./raw/team.json", "r") as f:
+    #     utils.output(os.path.join(data_dir, "team.json"), json.loads(f.read()))
+
+    while True:
+        log.info("loop start")
+
+        try:
+
+            # utils.output(os.path.join(data_dir, "run.json"), d.runs.get_dict)
+
+            log.info("work successfully")
+        except Exception as e:
+            log.error("work failed. ", e)
+
+        log.info("sleeping...")
+        time.sleep(5)
