49308194b7c964b2a3946a1216b82ec15df1e0ba
Dup4 <lyuzhi.pan@gmail.com>
2023-10-14 14:19:03 +0800

    feat: 9th ccpc qhd config
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/ccpc/9th/qinhuangdao/warmup.py
+++ origin-data/ccpc/9th/qinhuangdao/warmup.py
@@ -5,7 +5,7 @@ from xcpcio_board_spider import utils, logo
 import common
 
 DATA_DIR = os.getenv(
-    "DATA_DIR", "../../../../data/ccpc/2023/qinhuangdao")
+    "DATA_DIR", "../../../../data/ccpc/2023/qinhuangdao-warmup")
 
 team_urls = [
     "./raw/team.json",
@@ -19,10 +19,10 @@ run_urls = [
 def get_contest():
     c = common.get_basic_contest()
 
-    c.contest_name = "第九届中国大学生程序设计竞赛（秦皇岛）- 正式赛"
-    c.problem_quantity = 12
-    c.start_time = utils.get_timestamp_second("2023-10-15 09:00:00")
-    c.end_time = utils.get_timestamp_second("2023-10-15 14:00:00")
+    c.contest_name = "第九届中国大学生程序设计竞赛（秦皇岛）- 热身赛"
+    c.problem_quantity = 3
+    c.start_time = utils.get_timestamp_second("2023-10-14 15:00:00")
+    c.end_time = utils.get_timestamp_second("2023-10-14 17:30:00")
 
     c.fill_problem_id().fill_balloon_color()
 
