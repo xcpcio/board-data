8ce26b4d6fa9d2f10e51c010afb19f8029776a9c
Dup4 <lyuzhi.pan@gmail.com>
2023-10-15 09:10:48 +0800

    fix: problem nums
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/ccpc/9th/qinhuangdao/normal.py
+++ origin-data/ccpc/9th/qinhuangdao/normal.py
@@ -21,7 +21,7 @@ def get_contest():
     c = common.get_basic_contest()
 
     c.contest_name = "第九届中国大学生程序设计竞赛（秦皇岛）- 正式赛"
-    c.problem_quantity = 12
+    c.problem_quantity = 13
     c.start_time = utils.get_timestamp_second("2023-10-15 09:00:00")
     c.end_time = utils.get_timestamp_second("2023-10-15 14:00:00")
 
