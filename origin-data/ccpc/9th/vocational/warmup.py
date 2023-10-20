b22e5260226fd0bc84727f5999c007bc30d4cf3a
Dup4 <lyuzhi.pan@gmail.com>
2023-10-20 15:14:15 +0800

    feat: ccpc 9th girl and vocational
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/ccpc/9th/vocational/warmup.py
@@ -0,0 +1,32 @@
+import os
+
+from xcpcio_board_spider import utils
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/ccpc/9th/vocational-warmup")
+FETCH_URI = os.getenv(
+    "FETCH_URI", "http://ccpc.pintia.cn/v/js/")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "2023年中国大学生程序设计竞赛高职专场 - 热身赛"
+    c.problem_quantity = 3
+    c.start_time = utils.get_timestamp_second("2023-10-20 15:30:00")
+    c.end_time = utils.get_timestamp_second("2023-10-20 17:30:00")
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(DATA_DIR, FETCH_URI, c)
+
+
+if __name__ == "__main__":
+    main()
