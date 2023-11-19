a4937977d2af0c93b3107e0e0801a9f94e31546c
Dup4 <lyuzhi.pan@gmail.com>
2023-11-19 10:39:53 +0800

    feat: macau
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/icpc/48th/macau/formal.py
@@ -0,0 +1,33 @@
+import os
+
+from xcpcio_board_spider import utils
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/icpc/48th/macau")
+
+FETCH_URI = os.getenv(
+    "FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "第 48 届 ICPC 国际大学生程序设计竞赛区域赛澳门站 - 正式赛"
+    c.problem_quantity = 13
+    c.start_time = utils.get_timestamp_second("2023-11-19 09:00:00")
+    c.end_time = utils.get_timestamp_second("2023-11-19 14:00:00")
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(DATA_DIR, c, FETCH_URI)
+
+
+if __name__ == "__main__":
+    main()
