a66a15d95df0263b9b6dc6a324c2726f994b2475
Dup4 <lyuzhi.pan@gmail.com>
2024-01-12 10:53:30 +0800

    fix: remove start from name
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/icpc/48th/ecfinal/common.py
+++ origin-data/icpc/48th/ecfinal/common.py
@@ -47,6 +47,9 @@ def handle_teams(teams: Teams):
                 team.coach = members[3]
                 team.coach = team.coach.replace("(教练)", "")
 
+        if team.name.startswith("⭐"):
+            team.name.replace("⭐", "")
+
     for team_id in filter_team_ids:
         del teams[team_id]
 
