e634c2a39a05d36de5596161d9a861499a57cd68
Dup4 <lyuzhi.pan@gmail.com>
2024-01-23 08:01:34 +0800

    chore: ignore words
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,5 @@ module.exports = {
 
     "brace-style": "off",
     "@typescript-eslint/brace-style": ["error", "1tbs"],
-
-    "@unocss/order": "off",
-    "@unocss/order-attributify": "off",
   },
 };
