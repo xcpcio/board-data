5b3293cba694cb638d4bdd05179a0a6bdb41ae69
Dup4 <lyuzhi.pan@gmail.com>
2024-03-30 10:39:33 +0800

    feat: ccpc 9th final warmup
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/ccpc/9th/final/warmup.py
@@ -0,0 +1,33 @@
+import os
+
+from xcpcio_board_spider import utils, logo
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/ccpc/9th/final-warmup")
+
+FETCH_URI = os.getenv(
+    "FETCH_URI", "http://ccpc.pintia.cn/js/")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "第 9 届 CCPC 中国大学生程序设计竞赛总决赛 - 热身赛"
+    c.problem_quantity = 4
+    c.start_time = utils.get_timestamp_second("2024-3-30 10:00:00")
+    c.end_time = utils.get_timestamp_second("2024-3-30 12:00:00")
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(DATA_DIR, FETCH_URI, c)
+
+
+if __name__ == "__main__":
+    main()
