b1e304435836aa2b45c8e0264cfb7fff72101ae7
Dup4 <lyuzhi.pan@gmail.com>
2024-05-12 09:04:51 +0800

    feat: henan
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/provincial-contest/2024/henan/formal.py
+++ origin-data/provincial-contest/2024/henan/formal.py
@@ -5,20 +5,20 @@ from xcpcio_board_spider import utils
 import common
 
 DATA_DIR = os.getenv(
-    "DATA_DIR", "../../../../data/ccpc/9th/shenzhen")
+    "DATA_DIR", "../../../../data/provincial-contest/2024/henan")
 
 
 FETCH_URI = os.getenv(
-    "FETCH_URI", "https://cpc.csgrandeur.cn/rank/contests/127d0f90-dc48-43d8-95b1-163886d60698/")
+    "FETCH_URI", "https://cpc.csgrandeur.cn/rank/contests/cb59e47a-c935-436b-8661-8908abb52196/")
 
 
 def get_contest():
     c = common.get_basic_contest()
 
-    c.contest_name = "第 9 届 CCPC 中国大学生程序设计竞赛深圳站 - 正式赛"
+    c.contest_name = "第 6 届 CCPC 河南省大学生程序设计竞赛 - 正式赛"
     c.problem_quantity = 13
-    c.start_time = utils.get_timestamp_second("2023-11-12 09:00:00")
-    c.end_time = utils.get_timestamp_second("2023-11-12 14:00:00")
+    c.start_time = utils.get_timestamp_second("2024-05-12 09:10:00")
+    c.end_time = utils.get_timestamp_second("2024-05-12 14:10:00")
 
     c.fill_problem_id().fill_balloon_color()
 
