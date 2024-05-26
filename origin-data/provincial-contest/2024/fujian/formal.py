12868e3f288b923289c80b32a004fd44797a5db8
Dup4 <lyuzhi.pan@gmail.com>
2024-05-26 11:04:01 +0800

    fix: fujian
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/provincial-contest/2024/fujian/formal.py
+++ origin-data/provincial-contest/2024/fujian/formal.py
@@ -16,8 +16,8 @@ def get_contest():
 
     c.contest_name = "第十一届福建省大学生程序设计竞赛暨 CCPC 全国邀请赛（福州）"
     c.problem_quantity = 12
-    c.start_time = utils.get_timestamp_second("2024-05-26 9:00:00")
-    c.end_time = utils.get_timestamp_second("2024-05-26 14:00:00")
+    c.start_time = utils.get_timestamp_second("2024-05-26 9:30:00")
+    c.end_time = utils.get_timestamp_second("2024-05-26 14:30:00")
 
     c.fill_problem_id().fill_balloon_color()
 
