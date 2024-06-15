da9f1373e092e8a7821db78b9e420ce2cd979e0b
Dup4 <lyuzhi.pan@gmail.com>
2024-06-15 11:56:03 +0800

    feat: sichuan
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/provincial-contest/2024/sichuan/warmup.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+from xcpcio_board_spider import utils
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/provincial-contest/2024/sichuan-warmup")
+
+FETCH_URI = os.getenv(
+    "FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "第十六届四川省大学生程序设计竞套 - 热身赛"
+    c.problem_quantity = 4
+    c.start_time = utils.get_timestamp_second("2024-06-15 15:30:00")
+    c.end_time = utils.get_timestamp_second("2024-06-15 17:00:00")
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    data_dir = Path(DATA_DIR)
+    common.work(data_dir, c, FETCH_URI)
+
+
+if __name__ == "__main__":
+    main()
