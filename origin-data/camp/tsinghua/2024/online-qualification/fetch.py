03a7379414b584aab9bfea0c80f38448932cf69d
Dup4 <lyuzhi.pan@gmail.com>
2024-07-21 12:41:51 +0800

    feat: tsinghua
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/camp/tsinghua/2024/online-qualification/fetch.py
+++ origin-data/camp/tsinghua/2024/online-qualification/fetch.py
@@ -1,8 +1,11 @@
 import time
 import os
 from pathlib import Path
+import requests
+import bs4
+import copy
 
-from xcpcio_board_spider import logger, Contest, constants, logo, utils, Team, Teams, Submissions
+from xcpcio_board_spider import logger, Contest, constants, logo, utils, Team, Teams, Submissions, Submission
 from xcpcio_board_spider.type import Image
 
 log = logger.init_logger()
@@ -12,6 +15,7 @@ DATA_DIR = os.getenv(
     "DATA_DIR", "../../../../../data/camp/tsinghua/2024/online-qualification")
 FETCH_URI = os.getenv(
     "FETCH_URI", "")
+COOKIE = os.getenv("COOKIE", "")
 
 
 def get_basic_contest():
@@ -24,18 +28,116 @@ def get_basic_contest():
         constants.TEAM_TYPE_OFFICIAL: constants.TEAM_TYPE_ZH_CN_OFFICIAL,
         constants.TEAM_TYPE_UNOFFICIAL: constants.TEAM_TYPE_ZH_CH_UNOFFICIAL,
     }
+    c.organization = None
 
     return c
 
 
-def handle_teams(teams: Teams):
-    pass
+def fetch(uri: str) -> str:
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'cookie': COOKIE,
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    }
+    resp = requests.get(uri, headers=headers, timeout=10, verify=False)
+    if resp.status_code != 200:
+        raise RuntimeError(
+            f"fetch failed. [status_code={resp.status_code}]")
+    return resp.text
 
 
-def handle_runs(c: Contest, runs: Submissions):
-    t = utils.get_timestamp_second(
-        c.end_time) - utils.get_timestamp_second(c.start_time) - c.frozen_time
-    t = t * 1000
+def parse_teams(html: str) -> Teams:
+    teams = Teams()
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    tbody = soup.select('tbody')[0]
+    trs = tbody.select('tr')
+    for tr in trs:
+        if tr.has_attr('participantid'):
+            team_id = tr['participantid']
+        else:
+            continue
+        name = tr.select('td')[1].text.strip('\n ')
+        team = Team()
+        team.team_id = team_id
+        team.name = name
+        team.official = True
+        teams[team_id] = team
+    return teams
+
+
+def parse_submissions(html: str) -> Submissions:
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    tbody = soup.select('tbody')[0]
+    trs = tbody.select('tr')
+    submissions = Submissions()
+
+    for tr in trs:
+        if tr.has_attr('participantid'):
+            team_id = tr['participantid']
+        else:
+            continue
+
+        submission = Submission()
+        submission.team_id = team_id
+
+        for i in range(4, 17):
+            td = tr.select('td')[i]
+
+            _submission = copy.deepcopy(submission)
+            _submission.problem_id = i - 4
+
+            span = td.select('span')[0]
+
+            if span.get("class")[0] == "cell-rejected":
+                text = span.text
+
+                if '-' in text:
+                    num = int(text.split('-')[1])
+                else:
+                    num = 0
+
+                for j in range(num):
+                    __submission = copy.deepcopy(_submission)
+                    __submission.status = constants.RESULT_REJECTED
+                    __submission.timestamp = 0
+                    submissions.append(__submission)
+
+            elif span.get("class")[0] == "cell-accepted":
+                text = span.text
+
+                if text == '+':
+                    num = 0
+                else:
+                    num = int(text.split('+')[1])
+
+                timestamp_text = td.select('span')[1].text
+                h = int(timestamp_text.split(':')[0])
+                m = int(timestamp_text.split(':')[1])
+                timestamp = h * 60 * 60 + m * 60
+
+                __submission = copy.deepcopy(_submission)
+                __submission.timestamp = timestamp
+
+                for j in range(num):
+                    ___submission = copy.deepcopy(__submission)
+                    ___submission.status = constants.RESULT_REJECTED
+                    ___submission.timestamp = 0
+                    submissions.append(___submission)
+
+                ___submission = copy.deepcopy(__submission)
+                ___submission.status = constants.RESULT_CORRECT
+                submissions.append(___submission)
+    return submissions
 
 
 def write_to_disk(data_dir: str, c: Contest, teams: Teams, runs: Submissions, if_not_exists=False):
@@ -46,7 +148,7 @@ def write_to_disk(data_dir: str, c: Contest, teams: Teams, runs: Submissions, if
     utils.output(os.path.join(data_dir, "config.json"),
                  c.get_dict)
     utils.output(os.path.join(data_dir, "team.json"),
-                 teams, if_not_exists=if_not_exists)
+                 teams.get_dict, if_not_exists=if_not_exists)
     utils.output(os.path.join(data_dir, "run.json"),
                  runs.get_dict, if_not_exists=if_not_exists)
 
@@ -62,19 +164,21 @@ def work(c: Contest, data_dir: str, fetch_uri: str):
 
     while True:
         log.info("loop start")
-
         try:
+            resp = fetch(fetch_uri)
+            teams = parse_teams(resp)
+            submissions = parse_submissions(resp)
+            write_to_disk(data_dir, c, teams, submissions)
             log.info("work successfully")
         except Exception as e:
             log.error("work failed. ", e)
-
         log.info("sleeping...")
         time.sleep(5)
 
 
 if __name__ == "__main__":
     c = get_basic_contest()
-    c.contest_name = "Tsinghua Bootcamp 2024 - Online Qualification"
+    c.contest_name = "Tsinghua Bootcamp 2024. Qualification Round"
     c.problem_quantity = 13
     c.start_time = utils.get_timestamp_second("2024-07-21 13:00:00")
     c.end_time = utils.get_timestamp_second("2024-07-21 18:00:00")
