c9f81e61abc72b5a8f2e1a188bfb2999d7453581
Dup4 <lyuzhi.pan@gmail.com>
2024-10-21 09:09:27 +0800

    feat: harbin award
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/ccpc/10th/harbin/formal.py
+++ origin-data/ccpc/10th/harbin/formal.py
@@ -19,6 +19,7 @@ def get_contest():
     c.problem_quantity = 13
     c.start_time = utils.get_timestamp_second("2024-10-20 09:20:00")
     c.end_time = utils.get_timestamp_second("2024-10-20 14:20:00")
+    c.medal = "ccpc"
 
     c.fill_problem_id().fill_balloon_color()
 
