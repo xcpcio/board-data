d61645ab5338c7f27c26eb709fbbd68b97a09f75
Dup4 <lyuzhi.pan@gmail.com>
2024-11-09 10:12:12 +0800

    feat: hangzhou
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/icpc/49th/hangzhou/warmup.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+from xcpcio_board_spider import utils
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/icpc/49th/hangzhou-warmup")
+
+FETCH_URI = os.getenv(
+    "FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "第 49 届 ICPC 国际大学生程序设计竞赛区域赛杭州站 - 热身赛"
+    c.problem_quantity = 4
+    c.start_time = utils.get_timestamp_second("2024-11-09 14:00:00")
+    c.end_time = utils.get_timestamp_second("2024-11-09 16:30:00")
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(Path(DATA_DIR), c, FETCH_URI)
+
+
+if __name__ == "__main__":
+    main()
