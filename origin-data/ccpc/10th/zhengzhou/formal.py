df6af43ab948314e86d0d55af85c3404818570ca
Dup4 <lyuzhi.pan@gmail.com>
2024-11-16 18:29:10 +0800

    feat: shanghai
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/ccpc/10th/zhengzhou/formal.py
+++ origin-data/ccpc/10th/zhengzhou/formal.py
@@ -5,20 +5,20 @@ from xcpcio_board_spider import utils
 import common
 
 DATA_DIR = os.getenv(
-    "DATA_DIR", "../../../../data/ccpc/10th/chongqing")
+    "DATA_DIR", "../../../../data/ccpc/10th/zhengzhou")
 
 
 FETCH_URI = os.getenv(
-    "FETCH_URI", "https://cpc.csgrandeur.cn/rank/contests/40fb3fb6-402d-48fe-918d-64a0426d1dc9")
+    "FETCH_URI", "https://cpc.csgrandeur.cn/rank/contests/9de35784-ec9d-421e-8501-34353101a3ed")
 
 
 def get_contest():
     c = common.get_basic_contest()
 
-    c.contest_name = "第 10 届 CCPC 中国大学生程序设计竞赛重庆站 - 正式赛"
+    c.contest_name = "第 10 届 CCPC 中国大学生程序设计竞赛郑州站 - 正式赛"
     c.problem_quantity = 13
-    c.start_time = utils.get_timestamp_second("2024-11-10 09:20:00")
-    c.end_time = utils.get_timestamp_second("2024-11-10 14:20:00")
+    c.start_time = utils.get_timestamp_second("2024-11-17 09:00:00")
+    c.end_time = utils.get_timestamp_second("2024-11-17 14:00:00")
     c.medal = "ccpc"
 
     c.fill_problem_id().fill_balloon_color()
