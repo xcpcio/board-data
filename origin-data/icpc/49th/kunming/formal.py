c2b248ed830f328115192ccb874936f89c20e218
Dup4 <lyuzhi.pan@gmail.com>
2024-12-01 08:48:20 +0800

    feat: kunming
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/icpc/49th/kunming/formal.py
+++ origin-data/icpc/49th/kunming/formal.py
@@ -17,8 +17,8 @@ def get_contest():
 
     c.contest_name = "第 49 届 ICPC 国际大学生程序设计竞赛区域赛昆明站 - 正式赛"
     c.problem_quantity = 13
-    c.start_time = utils.get_timestamp_second("2024-12-01 09:00:00")
-    c.end_time = utils.get_timestamp_second("2024-12-01 14:00:00")
+    c.start_time = utils.get_timestamp_second("2024-12-01 09:30:00")
+    c.end_time = utils.get_timestamp_second("2024-12-01 14:30:00")
 
     c.fill_problem_id().fill_balloon_color()
 
