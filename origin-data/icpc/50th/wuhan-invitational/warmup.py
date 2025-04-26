5b7a8ca524d620dccb9b2ebc5b17ba8cdaff04e8
Dup4 <lyuzhi.pan@gmail.com>
2025-04-26 14:27:58 +0800

    fix: time
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/icpc/50th/wuhan-invitational/warmup.py
+++ origin-data/icpc/50th/wuhan-invitational/warmup.py
@@ -16,8 +16,8 @@ def get_contest():
 
     c.contest_name = "第 50 届 ICPC 国际大学生程序设计竞赛区域赛武汉站 - 热身赛"
     c.problem_quantity = 4
-    c.start_time = utils.get_timestamp_second("2025-04-27 14:45:00")
-    c.end_time = utils.get_timestamp_second("2025-04-27 17:15:00")
+    c.start_time = utils.get_timestamp_second("2025-04-26 14:45:00")
+    c.end_time = utils.get_timestamp_second("2025-04-26 17:15:00")
 
     c.fill_problem_id().fill_balloon_color()
 
