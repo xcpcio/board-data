df19ff6ccef9a9de61fe05f8569dc3ce1027cf6c
Dup4 <lyuzhi.pan@gmail.com>
2025-04-26 08:02:51 +0800

    feat: 2025 zjcpc
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/provincial-contest/2025/zhejiang/formal.py
@@ -0,0 +1,45 @@
+import os
+
+from xcpcio_board_spider import utils
+
+import common
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/provincial-contest/2025/zhejiang")
+FETCH_URI = os.getenv(
+    "FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "「睿琪杯」浙江省第 22 届大学生程序设计竞赛"
+    c.problem_quantity = 13
+    c.start_time = utils.get_timestamp_second("2025-04-26 12:00:00")
+    c.end_time = utils.get_timestamp_second("2025-04-26 17:00:00")
+
+    # c.medal = {
+    #     "undergraduate": {
+    #         "gold": 27,
+    #         "silver": 40,
+    #         "bronze": 66,
+    #     },
+    #     "vocational": {
+    #         "gold": 15,
+    #         "silver": 22,
+    #         "bronze": 36,
+    #     }
+    # }
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(c, DATA_DIR, FETCH_URI)
+
+
+if __name__ == "__main__":
+    main()
