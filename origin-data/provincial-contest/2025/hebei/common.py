8478304e902552d8cd4f19616a1ad2167c88449e
Dup4 <lyuzhi.pan@gmail.com>
2025-05-17 15:21:06 +0800

    fix: hebei
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- origin-data/provincial-contest/2025/hebei/common.py
+++ origin-data/provincial-contest/2025/hebei/common.py
@@ -1,8 +1,10 @@
 import json
+import time
 from pathlib import Path
 
-from xcpcio_board_spider import Contest, utils
+from xcpcio_board_spider import Contest, logger, utils
 
+log = logger.init_logger()
 
 def get_basic_contest():
     c = Contest()
@@ -16,9 +18,22 @@ def work(data_dir: Path, fetch_uri: str, c: Contest):
     utils.output(data_dir / "team.json", {}, True)
     utils.output(data_dir / "run.json", [], True)
 
-    if len(fetch_uri) > 0:
-        base_path = Path(fetch_uri)
-        for asset in ["config", "team", "run"]:
-            with open(base_path / f"{asset}.json", "r") as f:
-                utils.output(data_dir / f"{asset}.json",
-                             json.loads(f.read()), False)
+    if len(fetch_uri) == 0:
+        return
+
+    while True:
+        log.info("loop start")
+
+        try:
+            base_path = Path(fetch_uri)
+            for asset in ["config", "team", "run"]:
+                with open(base_path / f"{asset}.json", "r") as f:
+                    utils.output(data_dir / f"{asset}.json",
+                                json.loads(f.read()), False)
+
+            log.info("work successfully")
+        except Exception as e:
+            log.error("work failed. ", e)
+
+        log.info("sleeping...")
+        time.sleep(5)
