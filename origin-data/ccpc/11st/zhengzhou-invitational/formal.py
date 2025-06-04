fab990aa339268e2c29a95d3fe9abda1c883c460
Dup4 <lyuzhi.pan@gmail.com>
2025-06-04 22:10:27 +0800

    feat: zhengzhou-invitational
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/ccpc/11st/zhengzhou-invitational/formal.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+import common
+from xcpcio_board_spider import utils
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/ccpc/11st/zhengzhou-invitational")
+FETCH_URI = os.getenv("FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "2025 CCPC 全国邀请赛（郑州）暨第七届 CCPC 河南省赛 - 正式赛"
+    c.problem_quantity = 13
+    c.start_time = utils.get_timestamp_second("2025-05-18 09:00:00")
+    c.end_time = utils.get_timestamp_second("2025-05-18 14:00:00")
+
+    c.fill_problem_id().fill_balloon_color()
+    return c
+
+
+def main():
+    c = get_contest()
+    data_dir = Path(DATA_DIR)
+    common.work(data_dir, FETCH_URI, c)
+
+
+if __name__ == "__main__":
+    main()
