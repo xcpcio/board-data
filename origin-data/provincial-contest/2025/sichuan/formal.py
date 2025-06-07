a02cb9d1e83bba74ae2460a829e04cb88dc28ab7
Dup4 <lyuzhi.pan@gmail.com>
2025-06-07 10:29:03 +0800

    feat: sichuan
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ origin-data/provincial-contest/2025/sichuan/formal.py
@@ -0,0 +1,44 @@
+import os
+
+import common
+from xcpcio_board_spider import utils
+
+DATA_DIR = os.getenv(
+    "DATA_DIR", "../../../../data/provincial-contest/2025/sichuan")
+FETCH_URI = os.getenv(
+    "FETCH_URI", "")
+
+
+def get_contest():
+    c = common.get_basic_contest()
+
+    c.contest_name = "2025 四川省大学生程序设计竞赛 - 正式赛"
+    c.problem_quantity = 13
+    c.start_time = utils.get_timestamp_second("2025-06-08 09:00:00")
+    c.end_time = utils.get_timestamp_second("2025-06-08 14:00:00")
+
+    # c.medal = {
+    #     "undergraduate": {
+    #         "gold": 27,
+    #         "silver": 40,
+    #         "bronze": 67,
+    #     },
+    #     "vocational": {
+    #         "gold": 16,
+    #         "silver": 22,
+    #         "bronze": 38,
+    #     }
+    # }
+
+    c.fill_problem_id().fill_balloon_color()
+
+    return c
+
+
+def main():
+    c = get_contest()
+    common.work(c, DATA_DIR, FETCH_URI)
+
+
+if __name__ == "__main__":
+    main()
