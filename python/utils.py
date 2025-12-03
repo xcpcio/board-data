54b78045ffd60f74dadf7b1f8196e73dc7adb9c2
Dup4 <lyuzhi.pan@gmail.com>
2025-12-03 21:31:47 +0800

    refactor: gen index script
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ python/utils.py
@@ -0,0 +1,25 @@
+import json
+import os
+from pathlib import Path
+from typing import Dict
+
+
+def ensure_makedirs(path: Path):
+    path.mkdir(exist_ok=True)
+
+
+def json_input(path: str) -> Dict:
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def json_output(data):
+    return json.dumps(data)
+
+
+def output(target_path: Path, data: str, if_not_exists=False):
+    if if_not_exists and os.path.exists(target_path):
+        return
+
+    with open(target_path, "w") as f:
+        f.write(json_output(data))
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 # shellcheck disable=SC2068
 python3 \
   -m http.server \
-  -b 127.0.0.1 \
   $@ \
   8080
