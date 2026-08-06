d31ce643f621740ba1184c924856b0ee07feb8b1
Dup4 <lyuzhi.pan@gmail.com>
2026-08-06 20:43:21 +0800

    chore: remove rating data
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- src/rating-config/utils.ts
+++
@@ -1,7 +0,0 @@
-export function generateID(startYear: number, endYear: number) {
-  return `Asia-East-CPC-${startYear}-${endYear}`;
-}
-
-export function generateName(startYear: number, endYear: number) {
-  return `Rating for the ${startYear} - ${endYear} Season Asia East Collegiate Programming Contest`;
-}
